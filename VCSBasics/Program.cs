dbc5a40c6b92c2646ebacea3114b8ef89388a6b3
user07 <metehanyildiz.andac@gmail.com>
2020-09-15 17:36:33 +0300

    Change message

--- VCSBasics/Program.cs
+++ VCSBasics/Program.cs
@@ -6,7 +6,7 @@ namespace VCSBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Azure!");
+            Console.WriteLine("Hello Git!");
         }
     }
 }
